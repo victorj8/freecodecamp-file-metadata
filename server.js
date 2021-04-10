c170e26579aa2185c6568146dae1967bafdede5b
Victor Camacho <victor.camacho@block.one>
2021-04-10 15:03:24 -0400

    server code

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
 
 var app = express();
 
@@ -11,8 +14,9 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
